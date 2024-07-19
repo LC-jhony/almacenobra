8664828b1b12ac8eac5fe80ca3de38c09dc4ac91
LC-jhony <jhonyapm94@gmail.com>
2024-07-19 18:23:20 -0500

    update blade movement

--- resources/views/livewire/almacen/movements/create-output-material.blade.php
+++ resources/views/livewire/almacen/movements/create-output-material.blade.php
@@ -1,11 +1,14 @@
-<div>
+<x-bree.container>
     <form wire:submit="create">
         {{ $this->form }}
-
-        <button type="submit">
-            Submit
-        </button>
+        <div class="mt-4">
+            <x-filament::button type="submit">
+                Registrar movimiento
+            </x-filament::button>
+            <x-filament::button href="{{ route('list.movement') }}" tag="a" color="danger">
+                Cancelar
+            </x-filament::button>
+        </div>
     </form>
-
     <x-filament-actions::modals />
-</div>
+</x-bree.container>
